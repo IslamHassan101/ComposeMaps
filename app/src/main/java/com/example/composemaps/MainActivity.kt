1ac0c1d758bc64ae7db985ade75fbb7599dffbe9
IslamHassan <eslamwork2019@gmail.com>
2023-01-17 12:48:05 +0200

    add google maps

--- app/src/main/java/com/example/composemaps/MainActivity.kt
+++ app/src/main/java/com/example/composemaps/MainActivity.kt
@@ -1,18 +1,47 @@
 package com.example.composemaps
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.composemaps.ui.theme.ComposeMapsTheme
+import com.example.composemaps.ui.theme.greenColor
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.ktx.awaitMap
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
+
+
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,9 +51,96 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MapScreen()
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                backgroundColor = greenColor,
+                                title = {
+
+                                    Text(
+                                        text = "GFG",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center,
+                                        color = Color.White
+                                    )
+                                },
+                            )
+                        }) {
+
+                        mapUI(context = LocalContext.current)
+                    }
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun mapUI(context: Context) {
+
+    val mapView = rememberMapViewWithLifeCycle()
+
+    val locationName = remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 3.dp), horizontalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                value = locationName.value,
+                onValueChange = { locationName.value = it },
+                placeholder = { Text(text = "Enter Location") },
+                modifier = Modifier
+                    .padding(3.dp)
+                    .width(300.dp)
+                    .height(60.dp),
+                textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+                singleLine = true
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Button(onClick = {
+                getMapLocation(
+                    location = locationName.value,
+                    context = context,
+                    mapView = mapView
+                )
+            }, modifier = Modifier.padding(3.dp)) {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "search_icon")
+            }
+        }
+        AndroidView({ mapView }) { mapView ->
+            CoroutineScope(Dispatchers.Main).launch {
+                val map = mapView.awaitMap()
+                map.uiSettings.isZoomControlsEnabled = true
+            }
+        }
+
+    }
+
+}
+
+private fun getMapLocation(location: String, context: Context, mapView: MapView) {
+
+    var addressList: List<Address>? = null
+    mapView.getMapAsync {
+        if (location != null || location == "") {
+            val geocoder = Geocoder(context)
+            try {
+                addressList = geocoder.getFromLocationName(location, 1)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            val address: Address = addressList!![0]
+            val latLng = LatLng(address.latitude, address.longitude)
+            it.addMarker(MarkerOptions().position(latLng).title("Marker in$location"))
+            it.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 8f))
         }
     }
 }
--- app/src/main/java/com/example/composemaps/MapScreen.kt
+++ app/src/main/java/com/example/composemaps/MapScreen.kt
@@ -1,23 +1,28 @@
 package com.example.composemaps
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.location.Address
 import android.location.Geocoder
 import android.widget.Toast
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -28,39 +33,62 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import java.io.IOException
 
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-@Composable
-fun MapScreen() {
-
-    var mMap: GoogleMap? = null
-
-    val positions = listOf(
-        Positions("egypt", 30.03, 31.0),
-        Positions("egypt", 30.4, 31.5)
-    )
-
-    val cameraPositionState = rememberCameraPositionState {
-        positions.forEach {
-            position = CameraPosition.fromLatLngZoom(LatLng(it.lat, it.long), 10f)
-        }
-    }
-    Scaffold(topBar = {
-        TextField(value = "", onValueChange = {})
-    }) {
-        GoogleMap(cameraPositionState = cameraPositionState, onMyLocationButtonClick = { true }) {
-
-            positions.forEach { position ->
-                MarkerInfoWindow(
-                    state = MarkerState(LatLng(position.lat, position.long)),
-                ) {
-                    Text(
-                        text = position.title,
-                        fontWeight = FontWeight.Bold,
-                        style = MaterialTheme.typography.h4,
-                        color = MaterialTheme.colors.primary
-                    )
-                }
-            }
-        }
-    }
-}
+//@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+//@Composable
+//fun MapScreen() {
+//
+//    var addressList: List<Address>? = null
+//    val locationName = remember { mutableStateOf("") }
+//
+//    val context = LocalContext.current
+//    if (locationName.value != null || locationName.value == "") {
+//        val geocoder = Geocoder(context)
+//        try {
+//            addressList = geocoder.getFromLocationName(locationName.value, 1)
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//        }
+//
+//        val address: Address = addressList!![0]
+//        val latLng = LatLng(address.latitude, address.longitude)
+//        val cameraPositionState = rememberCameraPositionState {
+//            position = CameraPosition.fromLatLngZoom(latLng, 8f)
+//
+//        }
+//        Scaffold(topBar = {
+//            TextField(
+//                value = locationName.value,
+//                onValueChange = { locationName.value = it },
+//                placeholder = { Text(text = "Enter Location") },
+//                modifier = Modifier
+//                    .padding(3.dp)
+//                    .width(300.dp)
+//                    .height(60.dp),
+//                textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+//                singleLine = true
+//            )
+//            Button(onClick = {
+//                locationName.value
+//            }, modifier = Modifier.padding(3.dp)) {
+//                Icon(imageVector = Icons.Default.Search, contentDescription = "search_icon")
+//            }
+//        }) {
+//            GoogleMap(
+//                cameraPositionState = cameraPositionState
+//            ) {
+//
+//                MarkerInfoWindow(
+//                    state = MarkerState(latLng),
+//                ) {
+//                    Text(
+//                        text = locationName.value,
+//                        fontWeight = FontWeight.Bold,
+//                        style = MaterialTheme.typography.h4,
+//                        color = MaterialTheme.colors.primary
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
+//
